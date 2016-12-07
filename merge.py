ec09437cf72952772ac6544b19275f181c11d882
tlarrue <tlarrue2991@gmail.com>
2016-12-07 18:16:22 -0500

    dashed merge code

--- merge.py
+++ merge.py
@@ -3,16 +3,23 @@ import numpy as np
 if __name__ == '__main__':
 
 	f = open('./id_lookup.csv', 'rb')
-	lookup_data = np.genfromtxt(f, delimiter=',', names=True, case_sensitive=False, dtype=None) 
+	lookup_data = np.genfromtxt(f, delimiter=",", names=True, case_sensitive=False, dtype=None) 
 
 	f = open('./distance_matrix.csv', 'rb')
-	matrix = np.genfromtxt(f, delimiter=',', names=True, case_sensitive=False, dtype=None)
+	matrix = np.genfromtxt(f, delimiter=",", names=True, case_sensitive=False, dtype=None)
 	f.close()
 
-	print lookup_data.dtype.names
-	for ind,label in enumerate(matrix.dtype.names):
-		c = lookup_data['Class'][lookup_data['ID'] == label]
-		matrix.dtype.names[ind] = c
+	new_labels = []
+	for label in list(matrix.dtype.names):
 
-	np.savetxt('./distance_matrix_with_classes.csv', matrix, delimiter=",", 
-		header=",".join(i for i in array.dtype.names), comments="", fmt='%s')
+		try:
+			c = label + "-" + lookup_data['CLASS'][np.where(lookup_data['ID'] == label.replace("_", " "))][0]
+		except IndexError:
+			new_labels.append(label)
+			print "DELETE", label
+		else:
+			new_labels.append(c)
+
+	new_matrix = np.array(matrix, dtype=[(i,'f8') for i in new_labels])
+
+	np.savetxt('./distance_matrix_with_classes.csv', new_matrix, delimiter=",", header=",".join(i for i in new_matrix.dtype.names), comments="", fmt='%s')
